0c9d8c99036c0390d63e1c118e8060ed38c72ae0
Aditya Kulshrestha <kulshresthaaditya02@gmail.com>
2024-02-01 01:47:55 +0530

    Cleanup

--- customer-support-usecase-backend/adapter/adapter.py
+++ customer-support-usecase-backend/adapter/adapter.py
@@ -1,48 +1,5 @@
 from typing import List, Optional
 
-# Deprecated 
-# class adapter:
-#     def __init__(
-#         self,
-#         chunk_size: int = 500,
-#         chunk_overlap: int = 10,
-#         seperator: str = "\n\n",
-#         verbose: bool = True,
-#     ) -> str:
-#         self.chunk_size = chunk_size
-#         self.chunk_overlap = chunk_overlap
-#         self.seperator = seperator
-#         self.verboseprint = print if verbose else lambda *a: None
-
-#         self.verboseprint(
-#             f"ADAPTER: Adapter initialised successfully with the following configuration: chunk_size = {self.chunk_size}  chunk_overlap = {self.chunk_overlap}"
-#         )
-
-#     def _document_dictionary_to_lang_chain_document(self, text: str) -> Document:
-#         return Document(page_content=text, metadata={})
-
-#     def get_chunks(self, doc: list[str]):
-#         chunk_list = []
-
-#         for d in doc:
-#             text_splitter = RecursiveCharacterTextSplitter(
-#                 chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap
-#             )
-#             document = self._document_dictionary_to_lang_chain_document(
-#                 d,
-#             )
-#             texts = text_splitter.split_documents([document])
-
-#             for text in texts:
-#                 chunk_list.append(text.page_content)
-
-#         self.verboseprint(
-#             f"ADAPTER: Document chunking successful. No of chunks = {len(chunk_list)}"
-#         )
-#         return chunk_list
-
-
-# Version 2.0
 class Document:
     def __init__(self, page_content, metadata): 
         self.page_content = page_content 
